6a3a4fa06d0efa15b960a6bfc35dd965aa9fa881
ezepalleros <ezequiel.palleros@davinci.edu.ar>
2024-05-27 16:23:18 -0300

    Update MascotaControlador

--- src/controladores/MascotaControlador.java
+++ src/controladores/MascotaControlador.java
@@ -27,7 +27,7 @@ public class MascotaControlador implements MascotaRepository {
             ResultSet resultSet = statement.executeQuery();
        
             while (resultSet.next()) {
-            	Mascota mascota = new Mascota(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("variety"), resultSet.getString("type"), resultSet.getInt("age"), resultSet.getBoolean("vacuna"), resultSet.getString("carac"), resultSet.getString("diet"), resultSet.getBoolean("chip"), resultSet.getInt("adopt"));
+            	Mascota mascota = new Mascota(resultSet.getInt("codMas"), resultSet.getString("nomMas"), resultSet.getString("variMas"), resultSet.getString("tipoMas"), resultSet.getInt("edadMas"), resultSet.getInt("vacuMas"), resultSet.getString("caracterMas"), resultSet.getInt("dietMas"), resultSet.getInt("chipMas"), resultSet.getInt("adoptar"));
             	mascotas.add(mascota);
             }
         } catch (SQLException e) {
@@ -46,7 +46,7 @@ public class MascotaControlador implements MascotaRepository {
             ResultSet resultSet = statement.executeQuery();
             
             if (resultSet.next()) {
-            	mascotas = new Mascota(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("variety"), resultSet.getString("type"), resultSet.getInt("age"), resultSet.getBoolean("vacuna"), resultSet.getString("carac"), resultSet.getString("diet"), resultSet.getBoolean("chip"), resultSet.getInt("adopt"));
+            	mascotas = new Mascota(resultSet.getInt("codMas"), resultSet.getString("nomMas"), resultSet.getString("variMas"), resultSet.getString("tipoMas"), resultSet.getInt("edadMas"), resultSet.getInt("vacuMas"), resultSet.getString("caracterMas"), resultSet.getInt("dietMas"), resultSet.getInt("chipMas"), resultSet.getInt("adoptar"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -57,15 +57,15 @@ public class MascotaControlador implements MascotaRepository {
 	@Override
     public void addUser(Mascota mascota) {
         try {
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO users (name, variety, type, age, vacuna, carac, diet, chip, adopt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO users (nomMas, variMas, tipoMas, edadMas, vacuMas, caracterMas, dietMas, chipMas, adoptar) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
             statement.setString(1, mascota.getNomMas());
             statement.setString(2, mascota.getVariMas());
             statement.setString(3, mascota.getTipoMas());
             statement.setInt(4, mascota.getEdadMas());
-            statement.setBoolean(5, mascota.isVacuMas());
+            statement.setInt(5, mascota.getVacuMas());
             statement.setString(6, mascota.getCaracterMas());
-            statement.setString(7, mascota.getDietMas());
-            statement.setBoolean(8, mascota.isChipMas());
+            statement.setInt(7, mascota.getDietMas());
+            statement.setInt(8, mascota.getChipMas());
             statement.setInt(9, mascota.getAdoptar());
             
             int rowsInserted = statement.executeUpdate();
@@ -80,15 +80,15 @@ public class MascotaControlador implements MascotaRepository {
 	@Override
     public void updateUser(Mascota mascota) {
         try {
-            PreparedStatement statement = connection.prepareStatement("UPDATE users SET name = ?, variety = ?, type = ?, age = ?, vacuna = ?, carac = ?, diet = ?, chip = ?, adopt = ? WHERE id = ?");
+            PreparedStatement statement = connection.prepareStatement("UPDATE users SET nomMas = ?, variMas = ?, tipoMas = ?, edadMas = ?, vacuMas = ?, caracterMas = ?, dietMas = ?, chipMas = ?, adoptar = ? WHERE codMas = ?");
             statement.setString(1, mascota.getNomMas());
             statement.setString(2, mascota.getVariMas());
             statement.setString(3, mascota.getTipoMas());
             statement.setInt(4, mascota.getEdadMas());
-            statement.setBoolean(5, mascota.isVacuMas());
+            statement.setInt(5, mascota.getVacuMas());
             statement.setString(6, mascota.getCaracterMas());
-            statement.setString(7, mascota.getDietMas());
-            statement.setBoolean(8, mascota.isChipMas());
+            statement.setInt(7, mascota.getDietMas());
+            statement.setInt(8, mascota.getChipMas());
             statement.setInt(9, mascota.getAdoptar());
             
             int rowsUpdated = statement.executeUpdate();
@@ -103,7 +103,7 @@ public class MascotaControlador implements MascotaRepository {
     @Override
     public void deleteUser(int id) {
         try {
-            PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?");
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE codMas = ?");
             statement.setInt(1, id);
             
             int rowsDeleted = statement.executeUpdate();
